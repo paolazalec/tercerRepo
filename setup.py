b0855f229b3be1b80caef6f6c45a3890d95143b7
Paola Zalec <paolazalec@gmail.com>
2025-12-29 22:29:58 -0400

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Paola Zalec",                         # Tu nombre
+    author_email="paolazalec@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/paolazalec/tercerRepo",     # URL del proyecto
+)
